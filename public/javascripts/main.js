e8cabef581770400243437bb8fad8caba0dbd2b2
hiroju <cb.hiroto@gmail.com>
2013-04-06 14:49:15 +0900

    change whole layout, add close bottun

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,16 +4,16 @@ $(document).ready(function () {
         
         return {
             init: function () {
-                var dirs = $('#main .dir');
-                for (var i=0; i < dirs.length; i ++) {
-                    var path = dirs.eq(i).attr('path'); 
+                
+                $('#main .dir').each(function () {
+                    var path = $(this).attr('path');
                     ph.appendimg(path);
-                }
+                });
                 
-                for(var i=0; i < $('.dir').length; i ++) {
-                    ph.setUpload($('.dir').eq(i));
-                }
-                
+                $('.dir').each(function () {
+                    ph.setUpload($(this));
+                });
+
                 ph.hoverAction($('.content'));
             },
             
@@ -21,34 +21,52 @@ $(document).ready(function () {
                 $.getJSON('/dircheck?path='+path, function (json) {
                     var file =''
                     ,   div = ''
-                    ,   target = $('.dir[path="'+path+'"]');
-                    json.files.forEach(function (item) {
-                        
+                    ,   target = $('.dir[path="'+path+'"]')
+                    ,   parent_dir = json.dir_path;
+                    
+                    json.files.forEach(function (item) {    
                         //Create Content
                         if (item.isDirectory == true) {
                             //Create Directory Div
                             var name = $('<p>').text(item.name);
-                            div = $('<div>').addClass('content dir').attr('path', json.dir_path + '/' + item.name);
+                            var next_path = json.dir_path + '/' + item.name;
+                            div = $('<div>').addClass('content dir').attr('path', next_path);
                             console.log(json.dir_path);
                             div.append(name);
                             div = ph.setUpload(div);
+                            
+                            //image load event
                             file = div.one('click', function () {
-                                var next_path = $(this).attr('path');
+                                var close_btn = $('<div>').addClass('content close_btn').attr('data-group', next_path);
+                                $(close_btn).click(function () {
+                                    $('.content[data-group*="'+next_path+'"], [path*="'+next_path+'"]:not(.dir[path="'+next_path+'"])')
+                                    .hide('fast');
+                                    $(this).hide('fast');
+                                });
+                                $(this).after(close_btn);
                                 ph.appendimg(next_path);
+                                //dir close event
                                 $(this).click(function () {
-                                    $('.file[group="'+next_path+'"]').toggle('normal');
+                                    $('.content[data-group="'+next_path+'"], [path*="'+next_path+'"]').not(this).show('fast');
                                 });
                             });
+                            target.after($('<div>').addClass('clear'), file);
+                            
                         }else {
                             //Create Image Div
-                            var img = $('<img>').attr('src', '/file?path='+json.dir_path + '/' + item.name);
-                            div = $('<div>').addClass('content file').attr('group', path);
+                            var filepath = '/file?path='+json.dir_path + '/' + item.name;
+                            var img = $('<img>').addClass('img-polaroid lazy').attr({'src': '/img/grey.gif', 'data-original': filepath, 'data-group': path});
+                            div = $('<div>').addClass('content file').attr('data-group', path);
                             file = div.append(img);
+                            target.after(file);
                         }
                         
                         //Append Created Content
                         ph.hoverAction(file);
-                        target.after(file);
+                        
+                    });
+                    $("img[data-group='"+path+"']").lazyload({ 
+                        effect : "fadeIn"
                     });
                 });
             },
