ff6d5525b849956223272eb7738de7e154137458
hiroju <cb.hiroto@gmail.com>
2013-04-06 17:02:42 +0900

    fix input[file] cursor sensitive, add close-icon

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,13 +4,9 @@ $(document).ready(function () {
         
         return {
             init: function () {
-                
-                $('#main .dir').each(function () {
+                $('.dir').each(function () {
                     var path = $(this).attr('path');
                     ph.appendimg(path);
-                });
-                
-                $('.dir').each(function () {
                     ph.setUpload($(this));
                 });
 
@@ -21,8 +17,7 @@ $(document).ready(function () {
                 $.getJSON('/dircheck?path='+path, function (json) {
                     var file =''
                     ,   div = ''
-                    ,   target = $('.dir[path="'+path+'"]')
-                    ,   parent_dir = json.dir_path;
+                    ,   target = $('.dir[path="'+path+'"]');
                     
                     json.files.forEach(function (item) {    
                         //Create Content
@@ -31,40 +26,47 @@ $(document).ready(function () {
                             var name = $('<p>').text(item.name);
                             var next_path = json.dir_path + '/' + item.name;
                             div = $('<div>').addClass('content dir').attr('path', next_path);
-                            console.log(json.dir_path);
                             div.append(name);
                             div = ph.setUpload(div);
                             
                             //image load event
                             file = div.one('click', function () {
+                                //create close button
                                 var close_btn = $('<div>').addClass('content close_btn').attr('data-group', next_path);
+                                var close_icon = $('<i>').addClass('icon-double-angle-left');
+                                close_btn.append(close_icon);
+                                //set hide animation to close button
                                 $(close_btn).click(function () {
                                     $('.content[data-group*="'+next_path+'"], [path*="'+next_path+'"]:not(.dir[path="'+next_path+'"])')
                                     .hide('fast');
                                     $(this).hide('fast');
                                 });
+                                //add close button in this dir
                                 $(this).after(close_btn);
-                                ph.appendimg(next_path);
-                                //dir close event
+                                
+                                //set show event to dir
                                 $(this).click(function () {
                                     $('.content[data-group="'+next_path+'"], [path*="'+next_path+'"]').not(this).show('fast');
                                 });
+                                //load next image
+                                ph.appendimg(next_path);
                             });
-                            target.after($('<div>').addClass('clear'), file);
-                            
+                            //add created dir to clicked dir
+                            target.after($('<div>').addClass('clear'), file);                            
                         }else {
                             //Create Image Div
                             var filepath = '/file?path='+json.dir_path + '/' + item.name;
-                            var img = $('<img>').addClass('img-polaroid lazy').attr({'src': '/img/grey.gif', 'data-original': filepath, 'data-group': path});
+                            var img = $('<img>').addClass('img-polaroid lazy').attr({ 'data-original': filepath, 'data-group': path});
                             div = $('<div>').addClass('content file').attr('data-group', path);
                             file = div.append(img);
+                            //add created img to clicked dir
                             target.after(file);
                         }
                         
                         //Append Created Content
                         ph.hoverAction(file);
-                        
                     });
+                    //set lazyload event to load new image
                     $("img[data-group='"+path+"']").lazyload({ 
                         effect : "fadeIn"
                     });
